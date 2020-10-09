f402378ede374377dae4543322433f609dc98ff4
Juan Carlos Morales <juanc.morales@edu.uag.mx>
2020-10-09 10:05:03 -0500

    CapsOff al tiro

--- Practicas/Lab 01/GENFUN.C
+++ Practicas/Lab 01/GENFUN.C
@@ -1,8 +1,26 @@
 #include "stdio.h"
 #include"GENFUN.h"
-
-void main( void )
+void main( void  )
 {
-
+	printf("Testing  Genfun_vCapsoff\n");
+	uint8 au8BufferTest[12] = "HElLO WOrLd";
+	printf("Buffer before function%s\n",au8BufferTest );
+	Genfun_vCapsoff(&au8BufferTest[0],12);
+	printf("Buffer after function%s\n",au8BufferTest );
 }
-void Genfun_vCapsoff (unit8 *pu8Src, uint8 u8Sizeofflist)
+void Genfun_vCapsoff (uint8 *pu8Src, uint8 u8SizeOfflist)
+{
+	while( u8Sizeofflist !=0 )
+	{
+		if(*pu8Src>=ASCII_LOW_THRESHHOLD_CAPSON_TO_OFF && *pu8Src<=ASCII_HIGH_THRESHHOLD_CAPSON_TO_OFF)
+		{
+			*pu8Src+=ASCII_CONVERTION_FACTOR;
+		}
+		else
+		{
+			/*Nothing to do */
+		}
+		pu8Src++;
+	    u8Sizeofflist --;
+	}
+}
---
+++ Practicas/Lab 01/GENFUN.h
@@ -0,0 +1,13 @@
+
+typedef unsigned char  uint8;
+typedef unsigned short uint16;
+typedef unsigned int   uint32;
+
+/*Prototypes*/
+void Genfun_vCapsoff (uint8 *pu8Src, uint8 u8SizeOfflist);
+
+/*Definitions*/
+#define ASCII_HIGH_THRESHHOLD_CAPSON_TO_OFF     90
+#define ASCII_LOW_THRESHHOLD_CAPSON_TO_OFF      65
+#define ASCII_CONVERTION_FACTOR  				32
+
