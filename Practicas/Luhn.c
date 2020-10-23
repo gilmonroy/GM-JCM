ba9b74dba799d5eafe6e4dd3dbb33b84f000a360
Gil Monroy <gilmonroy01@gmail.com>
2020-10-22 20:28:17 -0700

    final

--- Practicas/Luhn.c
+++ Practicas/Luhn.c
@@ -1,41 +1,80 @@
-#include "stdio.h"
-int main(void) 
-{
-  printf("Ingresa tu numero de tarjeta > ");
-  char u8Numbers[16];
-  gets(u8Numbers);
-  printf("Numeros ingresados %s\n", u8Numbers);
-  verificar(u8Numbers);
-  return 0;
-}
+#include <stdio.h>
+#include <string.h>
 
-void verificar(char u8Numbers[])
+int main(void)
 {
-  int u8Operacion = 0,
-      u8Suma = 0;
-  //Ciclo para ir sumando los numeros ya sea multiplicados x2 o con su valor original segun 
-  //dice el algoritmo de Luhn
-  for(int i = 0; i < 16; i++)
-  {
-    if(i%2 == 0)
+    long NumeroDeTarjeta;
+
+    do
     {
-      u8Operacion = (u8Numbers[i]-'0') * 2;
-      if(u8Operacion >= 10)
-        u8Operacion = (u8Operacion - 10) + 1;
-    } 
+        printf("Please input CC number: \n");
+        scanf("%ld", &NumeroDeTarjeta);
+    }
+    while (NumeroDeTarjeta <= 0);
+
+    long NumeroValido = NumeroDeTarjeta;
+    int Sumatoria = 0;
+    int count = 0;
+    long divisor = 10;
+    char result[11];
+
+    while (NumeroValido > 0)
+    {
+        int UltimoNumeroDeSuTarjeta = NumeroValido % 10;
+        Sumatoria = Sumatoria + UltimoNumeroDeSuTarjeta;
+        NumeroValido = NumeroValido / 100;
+    }
+
+    NumeroValido = NumeroDeTarjeta / 10;
+    while (NumeroValido > 0)
+    {
+        int UltimoNumeroDeSuTarjeta = NumeroValido % 10;
+        int timesTwo = UltimoNumeroDeSuTarjeta * 2;
+        Sumatoria = Sumatoria + (timesTwo % 10) + (timesTwo / 10);
+        NumeroValido = NumeroValido / 100;
+    }
+
+    // length of the number / digit count
+    NumeroValido = NumeroDeTarjeta;
+    while (NumeroValido != 0)
+    {
+        NumeroValido = NumeroValido / 10;
+        count++;
+    }
+
+    // divisor
+    for (int i = 0; i < count - 2; i++)
+    {
+        divisor = divisor * 10;
+    }
+
+    int PrimerDigitoDeTuTarjeta = NumeroDeTarjeta / divisor;
+    int PrimerosDosDigitosDeTuTarjeta = NumeroDeTarjeta / (divisor / 10);
+
+    // final checks
+    if (Sumatoria % 10 == 0)
+    {
+        if (PrimerDigitoDeTuTarjeta == 4 && (count == 13 || count == 16))
+        {
+            strcpy(result, "VISA");
+        }
+        else if ((PrimerosDosDigitosDeTuTarjeta == 34 || PrimerosDosDigitosDeTuTarjeta == 37) && count == 15)
+        {
+            strcpy(result, "AMEX");
+        }
+        else if ((50 < PrimerosDosDigitosDeTuTarjeta && PrimerosDosDigitosDeTuTarjeta < 56) && count == 16)
+        {
+            strcpy(result, "MASTERCARD");
+        }
+        else 
+        {
+            strcpy(result, "Error");
+        }
+    }
     else
     {
-      u8Operacion = u8Numbers[i]-'0';
+        strcpy(result, "Error");
     }
-    u8Suma = u8Suma + u8Operacion;
-  }
-  //Si el resultado es multiplo de 10 entonces la sumatoria es correcta
-  if(u8Suma%10 == 0)
-  {
-    printf("Tarjeta valida %d\n", u8Suma);
-  }//%d la usamos para verificar que sean correctos las sumatorias
-  else 
-  {
-    printf("Tarjeta invalida %d\n", u8Suma);
-  }
+
+    printf("%s\n", result);
 }
